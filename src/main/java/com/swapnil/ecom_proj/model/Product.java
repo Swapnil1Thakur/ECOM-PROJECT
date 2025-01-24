1e3bd834f8b22ac92fdc87d2d1d11b2cf3e1ee4f
Swapnil <thakur7swapnil@gmail.com>
2025-01-24 21:27:40 +0530

    first commit

---
+++ src/main/java/com/swapnil/ecom_proj/model/Product.java
@@ -0,0 +1,166 @@
+package com.swapnil.ecom_proj.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Date;
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Product {
+
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+    private String brand;
+    private BigDecimal price;
+    private String category;
+
+
+    private Date releaseDate;
+    private boolean productAvailable;
+    private int stockQquantity;
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", brand='" + brand + '\'' +
+                ", price=" + price +
+                ", category='" + category + '\'' +
+                ", releaseDate=" + releaseDate +
+                ", productAvailable=" + productAvailable +
+                ", stockQquantity=" + stockQquantity +
+                ", imageName='" + imageName + '\'' +
+                ", imageType='" + imageType + '\'' +
+                ", imageDate=" + Arrays.toString(imageDate) +
+                '}';
+    }
+
+    private String imageName;
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    public String getImageType() {
+        return imageType;
+    }
+
+    public void setImageType(String imageType) {
+        this.imageType = imageType;
+    }
+
+    public byte[] getImageDate() {
+        return imageDate;
+    }
+
+    public void setImageDate(byte[] imageDate) {
+        this.imageDate = imageDate;
+    }
+
+    private String imageType;
+
+
+    @Lob
+    private byte[] imageDate;
+
+
+
+
+
+
+
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return description;
+    }
+
+    public void setDesc(String description) {
+        this.description = description;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public boolean isproductAvailable() {
+        return productAvailable;
+    }
+
+    public void setproductAvailable(boolean productAvailable) {
+        this.productAvailable = productAvailable;
+    }
+
+    public int getstockQquantity () {
+        return stockQquantity;
+    }
+
+    public void setstockQquantity(int stockQquantity) {
+        this.stockQquantity= stockQquantity;
+    }
+}
